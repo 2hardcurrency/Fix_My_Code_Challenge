7ab65f27e7814a8044bba1ac3ec7c66c137e0287
2hardcurrency <sinafavo@gmail.com>
2023-05-28 19:52:14 -0700

    my tasks

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,7 +6,6 @@
     of the program.
 */
 
-
 if (process.argv.length <= 2) {
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
@@ -16,9 +15,11 @@ if (process.argv.length <= 2) {
 
 const size = parseInt(process.argv[2]);
 
-for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
-        process.stdout.write("#");
+for (let i = 0; i < size; i++) {
+    let row = '';
+    for (let j = 0; j < size; j++) {
+        row += '#';
     }
-    process.stdout.write("\n");
+    console.log(row);
 }
+
